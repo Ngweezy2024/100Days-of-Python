dee049dc31468d0699ed348f12974966f63e3a75
Ngweezy2024 <erickwangoedt@gmail.com>
2024-05-29 09:26:14 +0300

    Add files via upload
    This program collects input or data from user and displays the data. It can be modified to perform extra operations on the data if the variables are changed. For example it could be made into a BMI calculator if it prompted user to input height and weight.

---
+++ user_input.py
@@ -0,0 +1,7 @@
+#Task: Write a program that asks the user for their name, age, and location
+#Then prints out a personalized message.
+name=input('Enter your name: ')
+age=input('Enter your age: ')
+location=input('Enter your location: ')
+print("Hello"+' ' + name+' ' + "you are"+' ' + age+' ' "years old and live in"+' ' + location)
+print('Lets welcome on stage' +' ' +name+' ' + 'who is' + ' '+age +' ' +'years old and has come all the way from' + ' '+ location+  ' '+ 'to join us today')
